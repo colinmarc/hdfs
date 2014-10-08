c660df3a901a6de3008262743607139a6f280fcb
Colin Marc <colinmarc@gmail.com>
2014-10-08 03:13:06 +0200

    use the right binary in minicluster.sh

--- minicluster.sh
+++ minicluster.sh
@@ -4,6 +4,7 @@ HADOOP_HOME=${HADOOP_HOME-"/usr/local/hadoop"}
 NN_PORT=${NN_PORT-"9000"}
 MINICLUSTER_JAR=$(find $HADOOP_HOME -name "hadoop-mapreduce-client-jobclient*.jar" | grep -v tests | head -1)
 
+echo $MINICLUSTER_JAR
 export HADOOP_NAMENODE="localhost:$NN_PORT"
 
-exec hadoop jar $MINICLUSTER_JAR minicluster -nnport $NN_PORT -nomr $@
+exec $HADOOP_HOME/bin/hadoop jar $MINICLUSTER_JAR minicluster -nnport $NN_PORT -nomr -format $@

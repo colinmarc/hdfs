b0fc1da417c78251c2d0d64078f4f7f6a80b0c64
Colin Marc <colinmarc@gmail.com>
2014-10-23 15:27:00 +0200

    implement ls -l

--- cmd/hdfs/ls.go
+++ cmd/hdfs/ls.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"github.com/colinmarc/hdfs"
 	"os"
+	"path"
 	"strings"
+	"text/tabwriter"
+	"time"
 )
 
 func ls(paths []string, long, all bool) {
@@ -41,7 +44,13 @@ func ls(paths []string, long, all bool) {
 	if len(files) == 0 && len(dirs) == 1 {
 		printDir(client, dirs[0], long, all)
 	} else {
-		printFiles(files, long, all)
+		var tw *tabwriter.Writer
+		if long {
+			tw = defaultTabWriter()
+			defer tw.Flush()
+		}
+
+		printFiles(tw, files, long, all)
 
 		for _, dir := range dirs {
 			fmt.Printf("\n%s/:\n", dir)
@@ -56,18 +65,68 @@ func printDir(client *hdfs.Client, dir string, long, all bool) {
 		fatal(err)
 	}
 
-	if all {
-		fmt.Println(".")
-		fmt.Println("..")
+	var tw *tabwriter.Writer
+	if long {
+		tw = defaultTabWriter()
+		defer tw.Flush()
 	}
 
-	printFiles(files, long, all)
-}
+	if all {
+		if long {
+			dot, err := stat(client, dir)
+			if err != nil {
+				fatal(err)
+			}
 
-func printFiles(files []os.FileInfo, long, all bool) {
-	for _, file := range files {
-		if all || !strings.HasPrefix(file.Name(), ".") {
-			fmt.Println(file.Name())
+			dotdot, err := stat(client, path.Join(dir, ".."))
+			if err != nil {
+				fatal(err)
+			}
+
+			printLong(tw, ".", dot)
+			printLong(tw, "..", dotdot)
+		} else {
+			fmt.Println(".")
+			fmt.Println("..")
 		}
 	}
+
+	printFiles(tw, files, long, all)
+}
+
+func printFiles(tw *tabwriter.Writer, files []os.FileInfo, long, all bool) {
+	for _, file := range files {
+		if all || !strings.HasPrefix(file.Name(), ".") {
+			if long {
+				printLong(tw, file.Name(), file)
+			} else {
+				fmt.Println(file.Name())
+			}
+		}
+	}
+}
+
+func printLong(tw *tabwriter.Writer, name string, info os.FileInfo) {
+	fi := info.(*hdfs.FileInfo)
+	// mode owner group size date(\w tab) time/year name
+	mode := fi.Mode().String()
+	owner := fi.Owner()
+	group := fi.OwnerGroup()
+	size := fi.Size()
+
+	modtime := fi.ModTime()
+	date := modtime.Format("Jan\t2")
+	var timeOrYear string
+	if modtime.Year() == time.Now().Year() {
+		timeOrYear = modtime.Format("15:04")
+	} else {
+		timeOrYear = string(modtime.Year())
+	}
+
+	fmt.Fprintf(tw, "%s \t%s \t %s \t %d \t%s \t%s \t%s\n",
+		mode, owner, group, size, date, timeOrYear, name)
+}
+
+func defaultTabWriter() *tabwriter.Writer {
+	return tabwriter.NewWriter(os.Stdout, 3, 0, 0, ' ', tabwriter.AlignRight|tabwriter.TabIndent)
 }

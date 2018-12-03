2aee68c73b644bd4c79ea6a7e02320e5ee53b94a
Praneeth Vallem <pvallem@tetrationanalytics.com>
2018-12-03 14:18:52 -0800

    Add overwrite option to rename.
    Summary:
       With overwrite option, HDFS namenode leaks the inodes, when dest file exists.
       ISSUE: https://issues.apache.org/jira/browse/HDFS-6870
    
       This PR, gives an option to user for overwriting.
    
    Test Plan:
       go test

--- rename.go
+++ rename.go
@@ -1,6 +1,7 @@
 package hdfs
 
 import (
+	"errors"
 	"os"
 
 	hdfs "github.com/colinmarc/hdfs/v2/internal/protocol/hadoop_hdfs"
@@ -25,6 +26,33 @@ func (c *Client) Rename(oldpath, newpath string) error {
 	err = c.namenode.Execute("rename2", req, resp)
 	if err != nil {
 		return &os.PathError{"rename", oldpath, interpretException(err)}
+	}
+
+	return nil
+}
+
+// RenameWithOverwrite renames (moves) a file. Overwrite option is taken as input.
+func (c *Client) RenameWithOverwriteOption(oldpath, newpath string, overwrite bool) error {
+	f, err := c.getFileInfo(newpath)
+	err = interpretException(err)
+	if err != nil && !os.IsNotExist(err) {
+		return &os.PathError{"rename", newpath, err}
+	}
+
+	if !overwrite && f != nil {
+		return &os.PathError{"rename", newpath, errors.New("Path exists.")}
+	}
+
+	req := &hdfs.Rename2RequestProto{
+		Src:           proto.String(oldpath),
+		Dst:           proto.String(newpath),
+		OverwriteDest: proto.Bool(overwrite),
+	}
+	resp := &hdfs.Rename2ResponseProto{}
+
+	err = c.namenode.Execute("rename2", req, resp)
+	if err != nil {
+		return &os.PathError{"rename_error", oldpath, interpretException(err)}
 	}
 
 	return nil

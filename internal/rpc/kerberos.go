a942f66ac10a447e567ea02e4318f8d200258982
Daisuke Taniwaki <daisuketaniwaki@gmail.com>
2019-02-27 17:54:10 +0900

    Set currentSessionKey

--- internal/rpc/kerberos.go
+++ internal/rpc/kerberos.go
@@ -56,6 +56,7 @@ func (c *NamenodeConnection) doKerberosHandshake() error {
 	if err != nil {
 		return err
 	}
+	c.currentSessionKey = sessionKey
 
 	if tokenAuth != nil {
 		challenge, err := ParseChallenge(tokenAuth)
